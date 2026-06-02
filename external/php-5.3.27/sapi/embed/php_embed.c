1b9c5fd4755438a5f5a615fdd91b4c745caadbe6
Link4Electronics <link4electronics@gmail.com>
2026-06-02 11:55:37 -0300

    Update php_embed.c

--- external/php-5.3.27/sapi/embed/php_embed.c
+++ external/php-5.3.27/sapi/embed/php_embed.c
@@ -244,6 +244,15 @@ __attribute__((weak))
 zend_php_scanner_globals language_scanner_globals;
 
 /*
+ * Same weak fallback for ini_scanner_globals, defined in
+ * zend_ini_scanner.c but also missing on powerpc64 ELFv2.
+ */
+#ifdef __GNUC__
+__attribute__((weak))
+#endif
+zend_ini_scanner_globals ini_scanner_globals;
+
+/*
  * Provide zend_isnan/zend_isinf/zend_finite on POSIX where
  * isnan/isinf are macros and finite is a libm function.
  */
