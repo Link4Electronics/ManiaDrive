398d726deb5f083445b57d5b05a681e20cff9f20
Link4Electronics <link4electronics@gmail.com>
2026-06-02 06:45:43 -0300

    fix zend compile php

--- external/php-5.3.27/Zend/zend_config.h
+++ external/php-5.3.27/Zend/zend_config.h
@@ -1,7 +1,12 @@
 #include <../main/php_config.h>
+#include <stdlib.h>
+#include <string.h>
 #if defined(APACHE) && defined(PHP_API_VERSION)
 #undef HAVE_DLFCN_H
 #endif
 #ifndef ZEND_API
 # define ZEND_API
+#endif
+#ifndef uint
+typedef unsigned int uint;
 #endif
